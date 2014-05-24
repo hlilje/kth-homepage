598aca2d25e0d9c35d7a3f1d56dfaeb87b375ffc
Hampus Liljekvist <hlilje@csc.kth.se>
2014-05-24 15:04:36 +0200

    Remove closing slash

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="The KTH homepage of Hampus Liljekvist">
     <meta name="keywords" content="Hampus Liljekvist,personal,homepage,KTH">
     <meta name="author" content="Hampus Liljekvist">
